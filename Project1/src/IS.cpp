99fb0ee8f244df93fd99156ef4d93c85a6daca00
mikl-o <ccorralw@asu.edu>
2022-09-18 15:48:09 -0700

    Makefile

--- Project1/src/IS.cpp
+++ Project1/src/IS.cpp
@@ -3,13 +3,10 @@
 //
 /*TODO this needs to be finished*/
 #include "IS.h"
-int *A,p,n;
+int *A,p,n,j;
 
-void insertion_sort(A[],n,p){S
-    for i := 2 to n do key := A[i]
-            //Insert A[i] into sorted
-            j:=i-1
-                    while j > 0 and A[j] > key do A[j+1] := A[j]
-                            j := j-1
-                                    A[j+1] := key
+void insertion_sort(int A[],int n, int p){
+    for (int i = 1; i <= n; i++)
+        do key : A[i]; j:i-1;
+                while (j > 0 and A[j] > key) do A[j+1] : A[j]:= j-1A[j+1] := key
 };
