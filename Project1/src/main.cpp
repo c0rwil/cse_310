e32c2d28a00d6324a51347a1819bf2dd911d7387
mikl-o <ccorralw@asu.edu>
2022-09-18 15:15:01 -0700

    Makefile

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -4,22 +4,25 @@
 /*TODO integrate this given code for main, after that start by getting functionality on a makefile w/ args to utilize
  * IS.h/IS.cpp first, then MS, then QS.*/
 #include <stdio.h>
-#include <stdlib.h>
-#include "IS.cpp"
+#include <stdlib.h>s
+#include "IS.h
 
 int main(int argc, char *argv[]){
     FILE *fp;
     int n, v1, v2, v3; float x; double y;
 
     printf("argc=%d\n", argc);
+
     for (int i=0; i<argc; i++){
         printf("The str value of argv[%d] is %s\n", i, argv[i]);
         printf("The int value of argv[%d] is %d\n\n", i, atoi(argv[i]));
     }
+
     if (argc < 3){
         printf("Usage: %s input_file output_file\n", argv[0]);
         exit (1);
     }
+
     fp = fopen(argv[1], "r");
     if (fp == NULL) {
         fprintf(stderr, "Error: cannot open file %s\n", argv[1]);
