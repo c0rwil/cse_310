18a2c1a3e0ca00b10ba5fb0989d57fef8456fd5a
mikl-o <ccorralw@asu.edu>
2022-10-14 17:13:22 -0700

    Makefile

---
+++ Project2/src/heap.h
@@ -0,0 +1,15 @@
+// ***************************************
+// * Carlos Corral-Williams | 1222280826 *
+// ***************************************
+
+
+#ifndef CSE_310_HEAP_H
+#define CSE_310_HEAP_H
+
+
+class heap {
+
+};
+
+
+#endif //CSE_310_HEAP_H
---
+++ Project2/src/util.h
@@ -0,0 +1,14 @@
+// ***************************************
+// * Carlos Corral-Williams | 1222280826 *
+// ***************************************
+
+#ifndef CSE_310_UTIL_H
+#define CSE_310_UTIL_H
+
+
+class util {
+
+};
+
+
+#endif //CSE_310_UTIL_H
