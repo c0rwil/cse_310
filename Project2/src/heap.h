3ddd725fcc7892d07c0c0974f5ab5f4a75ae2199
mikl-o <ccorralw@asu.edu>
2022-10-22 13:49:09 -0700

    Makefile

--- Project2/src/heap.h
+++ Project2/src/heap.h
@@ -3,13 +3,22 @@
 // ***************************************
 
 
-#ifndef CSE_310_HEAP_H
-#define CSE_310_HEAP_H
+#ifndef HEAP_H
+#define HEAP_H
 
-
-class heap {
-
+class TAG_ELEMENT{
+    int num;
+};
+class TAG_HEAP{
+    enum DataStructure {MaxHeap, MinHeap, DoubleHeap};
+    int cap; // capacity of the (max_heap)/(min_heap)/(double_ended_heap)
+    int len; // size of given heap
+    ELEMENT **A; // array of pointers to ELEMENT obj (for max)
+    ELEMENT **a // array of pointers to ELEMENT obj (for min)
+    public:
+        HEAP(adt);
+        HEAP(int,int);
 };
 
 
-#endif //CSE_310_HEAP_H
+#endif //HEAP_H
--- Project2/src/main.cpp
+++ Project2/src/main.cpp
@@ -1,3 +1,29 @@
 // ***************************************
 // * Carlos Corral-Williams | 1222280826 *
 // ***************************************
+
+// STD imports
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+
+// CUSTOM imports
+#include "heap.h"
+#include "util.h"
+
+using namespace std;
+TAG_ELEMENT *a;
+
+void makeArr(int size){
+    a = new TAG_ELEMENT[size];
+}
+int main(){
+    ifstream txt;
+    string commands;
+    int len;
+    txt.open("COMMANDS");
+    if(txt.is_open()){
+        txt >> commands;
+    }
+}
